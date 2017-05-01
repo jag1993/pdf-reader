3851610e7a1b076aac7812101755237f6ca33d61
Jose Alvaro Garcia <jag040393@gmail.com>
2017-04-30 20:27:18 -0700

    Tried doing map fix

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var writer = csvWriter({ headers: ["date","table", "pricing"]})
 const fs = require('fs');
 const noEmptyStrings = (n)=>{ return n != ' ' };
 const noEmptyArrays = (n)=> { for(i=0;i<n.length;i++){ 
-									if(n[i] ===  '' ){ 
-										return false 
-									}
-								} 
-										return true
-							}
+                  if(n[i] ===  '' ){ 
+                    return false 
+                  }
+                } 
+                    return true
+              }
 
 const ws = fs.createWriteStream('my.csv');
 
@@ -22,24 +22,39 @@ let eventsData =[];
 // STUDY ON MAP
 function success(result)
 {
-  result.pageTables.forEach((data)=>{
-  		data.tables.forEach((data)=>{
-  			data.forEach((data)=>{
-  				eventsData.push(data.split('\n').filter(noEmptyStrings));
-  			});
-  		});
-  })
+  // result.pageTables.forEach((data)=>{
+  //    data.tables.forEach((data)=>{
+  //      data.forEach((data)=>{
+  //        eventsData.push(data.split('\n').filter(noEmptyStrings));
+  //      });
+  //    });
+  // })
 
-  	let filteredValue = eventsData.filter(noEmptyArrays);
-    eventsData = filteredValue;
-    console.log(eventsData);
-  	
-  	writer.pipe(ws);
-  	eventsData.forEach((data)=>{ 
-  	//Find a way to parse through all the arrays taking the first two as the dates and event name
-  	//Then the rest of the values being TableName and Pricing alternatively.
-  	writer.write({date:data[0],table:data[1],pricing:data[2]})
-  	})
+  //  let filteredValue = eventsData.filter(noEmptyArrays);
+  //   eventsData = filteredValue;
+  //   console.log(eventsData);
+    
+  //  writer.pipe(ws);
+  //  eventsData.forEach((data)=>{ 
+  //  //Find a way to parse through all the arrays taking the first two as the dates and event name
+  //  //Then the rest of the values being TableName and Pricing alternatively.
+  //  writer.write({date:data[0],table:data[1],pricing:data[2]})
+  //  })
+
+  //value of pageTables key after extraction from PDF
+    let pageTables = result.pageTables;
+  //value of tables per page
+    let tables = pageTables.map(function(data){
+        let withEmptyArr = data.tables;
+        let noEmptyArr = withEmptyArr.filter(noEmptyArrays);
+       return noEmptyArr;
+    });
+    
+    tables.forEach((data)=>{
+      
+         eventsData.push(data.split('\n').filter(noEmptyStrings));
+       })
+
   }
  
 //Error 
